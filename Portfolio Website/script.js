7544232893e9e86c28ad1517f649f132523f8785
dahn821 <83702133+dahn821@users.noreply.github.com>
2021-08-12 09:23:12 -0700

    Add files via upload

---
+++ Portfolio Website/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Entrepreneur", "Tech Enthusiast", "Engineer", "Student, Thinker", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Entrepreneur", "Tech Enthusiast", "Engineer", "Student", "Thinker", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
